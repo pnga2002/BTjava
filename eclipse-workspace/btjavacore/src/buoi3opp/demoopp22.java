3560010444c70281c51e71281cdad716fbb389da
pnga2002 <phuongnga0pn@gmail.com>
2023-07-21 13:50:12 +0700

    push again

--- eclipse-workspace/btjavacore/src/buoi3opp/demoopp22.java
+++
@@ -1,9 +0,0 @@
-package buoi3opp;
-
-public class demoopp22 {
-	public static void main(String[] args) {
-		NhanVien nvA = new NhanVien();
-		nvA.setHoTen("Ho Thi Phuong Nga");
-		System.out.println("Ho Ten: " + nvA.getHoTen());
-	}
-}
